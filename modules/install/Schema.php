6182349f24ce0496c94c7428401cd45d292f2470
Petru Muraru <murarup@gmail.com>
2026-02-11 06:29:44 +0200

    fix schema

--- modules/install/Schema.php
+++ modules/install/Schema.php
@@ -1507,7 +1507,7 @@ class CATSSchema
                 }
             ',
             '368' => 'PHP:
-                $defaultBody = "* This is an automated message. Please do not reply. *\r\n%DATETIME%\r\n\r\nHello %CANDFIRSTNAME%,\r\n\r\nWe'd love to keep your application active and stay in touch about future opportunities at Avel Technologies but your privacy matters to us as much as your application does.\r\n\r\nTo ensure we're following the latest GDPR guidelines while we process your candidacy, we need your consent to keep your info in our secure system.\r\n\r\nClick here to stay on our radar: %CONSENT_LINK%\r\n\r\nThank you!\r\nThe Avel Technologies Team\r\n\r\nP.S. This link expires on %REQUEST_EXPIRES%, so don't let it sit in your inbox for too long!";
+                $defaultBody = "* This is an automated message. Please do not reply. *\r\n%DATETIME%\r\n\r\nHello %CANDFIRSTNAME%,\r\n\r\nWe\'d love to keep your application active and stay in touch about future opportunities at Avel Technologies but your privacy matters to us as much as your application does.\r\n\r\nTo ensure we\'re following the latest GDPR guidelines while we process your candidacy, we need your consent to keep your info in our secure system.\r\n\r\nClick here to stay on our radar: %CONSENT_LINK%\r\n\r\nThank you!\r\nThe Avel Technologies Team\r\n\r\nP.S. This link expires on %REQUEST_EXPIRES%, so don\'t let it sit in your inbox for too long!";
                 $possibleVariables = "%DATETIME%%SITENAME%%USERFULLNAME%%USERMAIL%%CANDIDATE_NAME%%CANDFIRSTNAME%%CANDFULLNAME%%CONSENT_LINK%%REQUEST_EXPIRES%";
 
                 $sql = sprintf(
@@ -1543,7 +1543,7 @@ class CATSSchema
                 $db->query($sql);
             ',
             '369' => 'PHP:
-                $updatedBody = "* This is an automated message. Please do not reply. *\r\n%DATETIME%\r\n\r\nHello %CANDFIRSTNAME%,\r\n\r\nWe'd love to keep your application active and stay in touch about future opportunities at Avel Technologies but your privacy matters to us as much as your application does.\r\n\r\nTo ensure we're following the latest GDPR guidelines while we process your candidacy, we need your consent to keep your info in our secure system.\r\n\r\nClick here to stay on our radar: %CONSENT_LINK%\r\n\r\nThank you!\r\nThe Avel Technologies Team\r\n\r\nP.S. This link expires on %REQUEST_EXPIRES%, so don't let it sit in your inbox for too long!";
+                $updatedBody = "* This is an automated message. Please do not reply. *\r\n%DATETIME%\r\n\r\nHello %CANDFIRSTNAME%,\r\n\r\nWe\'d love to keep your application active and stay in touch about future opportunities at Avel Technologies but your privacy matters to us as much as your application does.\r\n\r\nTo ensure we\'re following the latest GDPR guidelines while we process your candidacy, we need your consent to keep your info in our secure system.\r\n\r\nClick here to stay on our radar: %CONSENT_LINK%\r\n\r\nThank you!\r\nThe Avel Technologies Team\r\n\r\nP.S. This link expires on %REQUEST_EXPIRES%, so don\'t let it sit in your inbox for too long!";
                 $possibleVariables = "%DATETIME%%SITENAME%%USERFULLNAME%%USERMAIL%%CANDIDATE_NAME%%CANDFIRSTNAME%%CANDFULLNAME%%CONSENT_LINK%%REQUEST_EXPIRES%";
                 $sql = sprintf(
                     "UPDATE email_template
